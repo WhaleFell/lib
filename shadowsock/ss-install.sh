ae02465874d3ec6f48fdbcc2894a70b7e48f9ec2
AdminWhaleFall <2734184475@qq.com>
2022-03-23 00:49:39 +0800

    🎈 perf: ss-install 更改为加速地址.

--- shadowsock/ss-install.sh
+++ shadowsock/ss-install.sh
@@ -100,7 +100,8 @@ Download(){
 	fi
 	cd "${FOLDER}"
 	if [[ ${bit} == "x86_64" ]]; then
-		wget --no-check-certificate -N "https://github.com/shadowsocks/go-shadowsocks2/releases/download/${new_ver}/shadowsocks2-linux.gz"
+        # github.do 加速
+		wget --no-check-certificate -N "https://github.do/https://github.com/shadowsocks/go-shadowsocks2/releases/download/${new_ver}/shadowsocks2-linux.gz"
 	else
 		echo -e "${Error} Shadowsocks-Go版目前不支持 非64位 构架的服务器安装，请更换系统 !" && rm -rf "${FOLDER}" && exit 1
 	fi
@@ -114,7 +115,7 @@ Download(){
 }
 Service(){
 	if [[ ${release} = "centos" ]]; then
-		if ! wget --no-check-certificate "https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/service/ss_go_centos" -O /etc/init.d/ss-go; then
+		if ! wget --no-check-certificate "https://github.do/https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/service/ss_go_centos" -O /etc/init.d/ss-go; then
 			echo -e "${Error} Shadowsocks 服务管理脚本下载失败 !"
 			rm -rf "${FOLDER}"
 			exit 1
@@ -123,7 +124,7 @@ Service(){
 		chkconfig --add ss-go
 		chkconfig ss-go on
 	else
-		if ! wget --no-check-certificate "https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/service/ss_go_debian" -O /etc/init.d/ss-go; then
+		if ! wget --no-check-certificate "https://github.do/https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/service/ss_go_debian" -O /etc/init.d/ss-go; then
 			echo -e "${Error} Shadowsocks 服务管理脚本下载失败 !"
 			rm -rf "${FOLDER}"
 			exit 1
@@ -628,13 +629,13 @@ Set_iptables(){
 	fi
 }
 Update_Shell(){
-	sh_new_ver=$(wget --no-check-certificate -qO- -t1 -T3 "https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/ss-go.sh"|grep 'sh_ver="'|awk -F "=" '{print $NF}'|sed 's/\"//g'|head -1) && sh_new_type="github"
+	sh_new_ver=$(wget --no-check-certificate -qO- -t1 -T3 "https://github.do/https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/ss-go.sh"|grep 'sh_ver="'|awk -F "=" '{print $NF}'|sed 's/\"//g'|head -1) && sh_new_type="github"
 	[[ -z ${sh_new_ver} ]] && echo -e "${Error} 无法链接到 Github !" && exit 0
 	if [[ -e "/etc/init.d/ss-go" ]]; then
 		rm -rf /etc/init.d/ss-go
 		Service
 	fi
-	wget -N --no-check-certificate "https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/ss-go.sh" && chmod +x ss-go.sh
+	wget -N --no-check-certificate "https://github.do/https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/ss-go.sh" && chmod +x ss-go.sh
 	echo -e "脚本已更新为最新版本[ ${sh_new_ver} ] !(注意：因为更新方式为直接覆盖当前运行的脚本，所以可能下面会提示一些报错，无视即可)" && exit 0
 }
 check_sys
