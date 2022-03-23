c91d64ae0fd7d0360865fee576c92488748a1114
蓝鲸落 <whalefall9420@outlook.com>
2022-03-23 21:51:54 +0800

    fix: LAN-proxy-PAC.js
    修复变量名BUG，增加不需要代理的内网ip列表。

--- LAN-proxy-PAC.js
+++ LAN-proxy-PAC.js
@@ -2,7 +2,10 @@
 // 适合通过链接回家
 // PAC has no v6 support, it sucks
 var ip4Re = /^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$/
+var proxy = __PROXY__;
+var direct = 'DIRECT;';
 
+// 需要代理访问的内网地址
 var privateNet = [
     ["10.0.0.0", "255.0.0.0"],
     ["127.0.0.0", "255.0.0.0"],
@@ -10,10 +13,23 @@ var privateNet = [
     ["192.168.0.0", "255.255.0.0"],
 ]
 
+// 不需要代理访问的内网地址(当前所处内网)
+var currentNet = [
+    ["192.168.5.0", "225.225.225.0"]
+]
+
 function FindProxyForURL(url, host) {
     if (host.match(ip4Re)) {
+        // 需要登录光猫时使用.
+        // if (host == "192.168.1.1") return proxy;
+        for (var a = 0; a < currentNet.length; a++) {
+            // isInNet 判断主机IP是否在指定子网内
+            // 先判断所处内网
+            if (isInNet(host, currentNet[a][0], currentNet[a][1])) return direct;
+        }
         for (var i = 0; i < privateNet.length; i++) {
             // isInNet 判断主机IP是否在指定子网内
+            // 再判断需要代理的内网ip
             if (isInNet(host, privateNet[i][0], privateNet[i][1])) return proxy;
         }
     }
